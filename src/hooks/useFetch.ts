fc5d602280e698c671ad8347dd1a1da27ae3955e
Nieopodal <mail.macieja.poltorak@gmail.com>
2023-03-04 18:16:32 +0100

    Added universal hook to fetch data from an API.

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,38 @@
+import {useEffect, useState} from "react";
+import { ApiResponse, OperationEntity, PeriodEntity } from "types";
+
+type FetchedData = OperationEntity | OperationEntity[] | PeriodEntity[] | PeriodEntity;
+export const useFetch = (url: string) => {
+
+    const [data, setData] = useState<FetchedData | null>(null);
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                setLoading(true);
+                const res = await fetch(`http://localhost:3001/${url}`);
+                const fetchedData: ApiResponse<FetchedData | null> = await res.json();
+
+                if (fetchedData.success) {
+                    if (fetchedData.payload === null) {
+                        setData(null);
+                    } else {
+                        setData(fetchedData.payload);
+                    }
+                } else if (fetchedData.error) {
+                    setError(fetchedData.error);
+                } else {
+                    setError('Wystąpił nieznany błąd serwera. Spróbuj później.');
+                }
+            } catch (e) {
+                setError(`Wystąpił błąd podczas próby wykonania zapytania.`);
+            } finally {
+                setLoading(false);
+            }
+        })();
+    }, [url]);
+
+    return [data, error, loading];
+};
