4f939a4f83924fe625bc2482769512caa12a8415
Nieopodal <mail.macieja.poltorak@gmail.com>
2023-03-14 13:37:21 +0100

    Added user-slice to redux and mocked its data.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Route, Routes} from "react-router-dom";
-import {Provider} from "react-redux";
+import {useDispatch} from "react-redux";
 import {ToastContainer} from "react-toastify";
 import {Header} from "../Header/Header";
 import {Dashboard} from "../Dashboard/Dashboard";
@@ -8,18 +8,24 @@ import {Footer} from "../Footer/Footer";
 import {OneOperation} from "../OneOperation/OneOperation";
 import {AllOperations} from "../AllOperations/AllOperations";
 import {AddOperation} from "../AddOperation/AddOperation";
-import {store} from "../../store";
 
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
+import {setDefaultBudgetAmount, setFinancialCushion} from "../../features/user/user-slice";
 
 export const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(setFinancialCushion(34543.44));
+        dispatch(setDefaultBudgetAmount(5000));
+    }, [dispatch]);
 
     return <div className="">
 
         <Header/>
         <ToastContainer autoClose={10000}/>
-        <Provider store={store}>
+
             <Routes>
                 <Route path="/" element={<Dashboard/>}/>
                 <Route path="/add-operation" element={<AddOperation/>}/>
@@ -27,7 +33,7 @@ export const App = () => {
                 <Route path="/operation/:operationId" element={<OneOperation/>}/>
             </Routes>
 
-        </Provider>
+
         <Footer/>
     </div>
 };
--- src/components/Dashboard/FinancialCushionCard.tsx
+++ src/components/Dashboard/FinancialCushionCard.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import {Card} from "../common/Card";
 import {pricifyHandler} from "../../utils/pricify-handler";
+import {useSelector} from "react-redux";
+import {RootState} from "../../store";
 
 export const FinancialCushionCard = () => {
+    const {actualUser} = useSelector((state: RootState) => state.user);
 
     return <Card>
         <div className="pl-7 pt-7 pb-8 mx-auto sm:mx-0 w-fit sm:w-auto">
             <div className="stat-title xl:text-lg font-semibold ">
                 <h3>Poduszka finansowa [PLN]</h3>
             </div>
-            <div className="stat-value">{pricifyHandler(54503.45)}</div>
+            <div className="stat-value">{pricifyHandler(actualUser.financialCushion)}</div>
         </div>
     </Card>
 };
