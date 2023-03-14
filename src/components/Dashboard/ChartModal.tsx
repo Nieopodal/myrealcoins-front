a444e89fbc7521c789b6b3b2556791ca3900acc9
Nieopodal <mail.macieja.poltorak@gmail.com>
2023-03-14 21:04:19 +0100

    Added colors into ModalChart based on user's theme.

--- src/components/Dashboard/ChartModal.tsx
+++ src/components/Dashboard/ChartModal.tsx
@@ -1,10 +1,11 @@
 import {Modal} from "../common/Modal";
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Chart} from "../common/Chart";
 import {useSelector} from "react-redux";
 import {RootState} from "../../store";
 import {getPaymentsCategoriesAndAmountsHandler} from "../../utils/get-payment-categories-amounts-handler";
 import ThreeDots from "../common/Loader";
+
 
 interface Props {
     open: boolean;
@@ -19,8 +20,33 @@ export const ChartModal = ({open, handleToggle}: Props) => {
     const [amounts, setAmounts] = useState<number[]>([]);
     const [colors, setColors] = useState<string[]>(['#0066FF', '#FF0074', '#FF7244', '#FFBE3A',]);
     // '#4C8076', '#D70000', '#7C7484', '#372B47', '#E5F3FA', '#560DF6'
+    const neutral = window.getComputedStyle(document.body).getPropertyValue('--n');
+    const primary = window.getComputedStyle(document.body).getPropertyValue('--p');
+    const warning = window.getComputedStyle(document.body).getPropertyValue('--wa');
+    const success = window.getComputedStyle(document.body).getPropertyValue('--su');
+
+    const neutralRef = useRef<HTMLDivElement>(null);
+    const primaryRef = useRef<HTMLDivElement>(null);
+    const warningRef = useRef<HTMLDivElement>(null);
+    const successRef = useRef<HTMLDivElement>(null);
+
+
     useEffect(() => {
         try {
+            if (neutralRef.current &&
+                primaryRef.current &&
+                warningRef.current &&
+                successRef.current
+            ) {
+                setColors([
+                    neutralRef.current.style.backgroundColor,
+                    primaryRef.current.style.backgroundColor,
+                    warningRef.current.style.backgroundColor,
+                    successRef.current.style.backgroundColor,
+                ])
+            }
+
+
             const result = getPaymentsCategoriesAndAmountsHandler(operations);
             setAmounts(result.amounts);
             setCategories(result.categories);
@@ -41,12 +67,31 @@ export const ChartModal = ({open, handleToggle}: Props) => {
             <h3 className="font-bold text-lg w-fit mx-auto">
                 Struktura wydatków
             </h3>
+            <div className="hidden">
+            <div ref={neutralRef} style={{
+                background: `hsl(${neutral})`,
+            }}>
+            </div>
+            <div ref={primaryRef} style={{
+                background: `hsl(${primary})`,
+            }}>
+            </div>
+            <div ref={warningRef} style={{
+                background: `hsl(${warning})`,
+            }}>
+            </div>
+            <div ref={successRef} style={{
+                background: `hsl(${success})`,
+            }}>
+            </div>
+            </div>
+
 
             <div className="py-4 w-fit mx-auto">
                 {loading && <ThreeDots/>}
 
-                {!loading &&<Chart labels={categories} amounts={amounts}
-                       backgroundColors={colors}/>}
+                {!loading && <Chart labels={categories} amounts={amounts}
+                                    backgroundColors={colors}/>}
             </div>
 
             <div className="modal-action">
