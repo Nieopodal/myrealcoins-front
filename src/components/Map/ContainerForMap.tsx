e0894b6a21d2c3567c92879b3507b66e8bf2ed10
Nieopodal <mail.macieja.poltorak@gmail.com>
2023-03-28 21:35:56 +0200

    Added the ability to view payment locations on the map.

---
+++ src/components/Map/ContainerForMap.tsx
@@ -0,0 +1,45 @@
+import {AllOperations} from "../AllOperations/AllOperations";
+import {Map} from "./Map";
+import React, {useEffect, useState} from "react";
+import {showToast, Toast} from "../../utils/show-toast";
+
+interface UserGeolocation {
+    lat: null | number;
+    lon: null | number;
+}
+
+export const ContainerForMap = () => {
+
+    const [userGeolocation, setUserGeolocation] = useState<UserGeolocation>({
+        lat: null,
+        lon: null,
+    });
+
+    useEffect(() => {
+
+        navigator.geolocation.watchPosition((position) => {
+                setUserGeolocation({
+                    lat: position.coords.latitude,
+                    lon: position.coords.longitude,
+                })
+            },
+            (error) => {
+                switch (error.code) {
+                    case error.PERMISSION_DENIED:
+                        showToast(Toast.Error, "Lokalizacja zablokowana przez użytkownika.");
+                        break;
+                    case error.POSITION_UNAVAILABLE:
+                        showToast(Toast.Error, "Lokalizacja jest niedostępna.");
+                        break;
+                }
+            },
+            {
+                enableHighAccuracy: true,
+                timeout: 5000,
+            });
+    }, [userGeolocation]);
+
+    return <AllOperations>
+        <Map centerLat={userGeolocation.lat} centerLon={userGeolocation.lon}/>
+    </AllOperations>
+};
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,6 +1,29 @@
-export const Map = () => {
+import {MapContainer, TileLayer} from "react-leaflet";
+import '../../utils/fix-map-icon';
+import './Map.css';
+import {useContext} from "react";
+import {AllOperationsContext} from "../../contexts/all-operations.context";
+import {OneMarker} from "./OneMarker";
+import 'leaflet/dist/leaflet.css';
 
-    return <div>
-        Mapka
+import ThreeDots from "../common/Loader";
+interface Props {
+    centerLat?: number | null;
+    centerLon?: number | null;
+}
+export const Map = ({centerLat, centerLon}: Props) => {
+    const {operations, loading} = useContext(AllOperationsContext);
+
+    return <div className="map">
+        {loading && <ThreeDots/>}
+
+        {!loading &&
+            <MapContainer center={centerLat && centerLon ? [centerLat, centerLon] : [52.0403662, 19.9563874]} zoom={centerLat && centerLon ? 13 : 6}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'/>
+
+                {operations && operations.map(operation => <OneMarker operation={operation} key={operation.id}/>)}
+            </MapContainer>
+        }
     </div>
 };
