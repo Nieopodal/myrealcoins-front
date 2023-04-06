22a20ac752473e12d3fc5deb7897bbdb8b8169c0
Nieopodal <mail.macieja.poltorak@gmail.com>
2023-04-06 12:13:53 +0200

    Added home page.

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,8 @@
 import useFindUser from "../hooks/useFindUser";
-import {useNavigate} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import {useEffect} from "react";
+
+import './Home.css';
 
 export const Home = () => {
     const navigate = useNavigate();
@@ -8,6 +10,19 @@ export const Home = () => {
 
     useEffect(() => {
         if (user) navigate('/dashboard', {replace: true});
-    },[user]);
-    return <div>Hello! strona główna</div>
+    }, [user]);
+    return <div className="hero min-h-screen bg">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">MyRealCoins</h1>
+                <p className="mb-5">Uporządkuj swój budżet. Zaplanuj przyszłość. Bogać się. Korzystaj za darmo.</p>
+                <NavLink to="/register">
+                    <button className="btn btn-primary">Zaczynamy</button>
+                </NavLink>
+            </div>
+        </div>
+    </div>
 };
+
+//Photo by Tyler Franta on Unsplash
