6dc9810fe12a056f842a31118289f8173087fe1a
Nieopodal <mail.macieja.poltorak@gmail.com>
2023-04-07 13:26:02 +0200

    Small code refactor.

--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,12 @@ import {App} from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
-
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-
-        <BrowserRouter>
-
-            <App/>
-
-        </BrowserRouter>
-
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
